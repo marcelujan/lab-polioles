69e20a175c6d37975ca94611dba7d3788d75792f
marcelujan <marcelo.lujan@mi.unc.edu.ar>
2025-04-08 12:52:00 -0300

    Actualización: se actualizó firebase_config y se modificó el flujo de recarga

--- firebase_config.py
+++ firebase_config.py
@@ -4,8 +4,18 @@ import streamlit as st
 import json
 
 def iniciar_firebase():
+    # Si ya hay una app inicializada, simplemente devolvemos el cliente de Firestore.
     if not firebase_admin._apps:
-        cred_dict = json.loads(st.secrets["firebase_key"])
-        cred = credentials.Certificate(cred_dict)
-        firebase_admin.initialize_app(cred)
+        try:
+            # Obtén el string desde los secrets
+            secret_str = st.secrets["firebase_key"]
+            # Parsear el JSON
+            cred_dict = json.loads(secret_str)
+            # Crear las credenciales con el diccionario obtenido
+            cred = credentials.Certificate(cred_dict)
+            # Inicializar la app con las credenciales
+            firebase_admin.initialize_app(cred)
+        except Exception as e:
+            st.error("Error al inicializar Firebase: " + str(e))
+            raise e
     return firestore.client()

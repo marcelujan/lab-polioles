d2e8cee4a47b9f1f59de6bfb91243dec1514d4f8
marcelujan <marcelo.lujan@mi.unc.edu.ar>
2025-04-29 09:45:26 -0300

    Actualizaci├│n autom├ítica desde script

--- app.py
+++ app.py
@@ -451,7 +451,7 @@ with tab4:
         if len(observaciones) > 80:
             observaciones = observaciones[:77] + "..."
 
-        nombre_espectro = f"{fecha} - {observaciones}"
+        nombre_espectro = f"{row['Muestra']} – {row['Tipo']} – {fecha} – {observaciones}"
         espectros_info.append({
             "identificador": idx,  # el índice real en df_filtrado
             "nombre": nombre_espectro

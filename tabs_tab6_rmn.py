d536c359d612b05c6741e91ff31a7a01cf7f8710
marcelujan <marcelo.lujan@mi.unc.edu.ar>
2025-05-15 14:28:39 -0300

    Actualizaci├│n autom├ítica desde script

--- tabs_tab6_rmn.py
+++ tabs_tab6_rmn.py
@@ -85,11 +85,15 @@ def render_tab6(db, cargar_muestras, guardar_muestra, mostrar_sector_flotante):
         fig, ax = plt.subplots()
 
         for idx, (_, row) in enumerate(df_rmn1H.iterrows()):
+                if not usar_mascara.get(row['id'], False):
+                    continue
             color = colores[idx % len(colores)]
-            usar_mascara[row["id"]] = st.checkbox(f"{row['muestra']} – {row['archivo']}", value=False, key=f"chk_mask_{row['id']}")
+            usar_mascara[row["id"]] = st.checkbox(f"{row['muestra']} – {row['archivo']}", value=False, key=f"chk_mask_{row['id']}_{idx}")
 
         # Gráfico primero
         for idx, (_, row) in enumerate(df_rmn1H.iterrows()):
+                if not usar_mascara.get(row['id'], False):
+                    continue
             color = colores[idx % len(colores)]
             try:
                 contenido = BytesIO(base64.b64decode(row["contenido"]))
@@ -132,6 +136,8 @@ def render_tab6(db, cargar_muestras, guardar_muestra, mostrar_sector_flotante):
                 "Xmax": float(df_asignacion_edit.iloc[0]["X máximo"])}
 
             for idx, (_, row) in enumerate(df_rmn1H.iterrows()):
+                if not usar_mascara.get(row['id'], False):
+                    continue
                 color = colores[idx % len(colores)]
                 try:
                     contenido = BytesIO(base64.b64decode(row["contenido"]))

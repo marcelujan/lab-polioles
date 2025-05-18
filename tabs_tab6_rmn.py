1b2b7607d853c68a1a3d996080f5b2bbf4813bc7
marcelujan <marcelo.lujan@mi.unc.edu.ar>
2025-05-18 16:21:10 -0300

    Actualizaci├│n autom├ítica desde script

--- tabs_tab6_rmn.py
+++ tabs_tab6_rmn.py
@@ -413,20 +413,6 @@ def render_tab6(db, cargar_muestras, guardar_muestra, mostrar_sector_flotante):
 
         st.rerun()
 
-        # Exportar a Excel
-        excel_buffer = io.BytesIO()
-        with pd.ExcelWriter(excel_buffer, engine="xlsxwriter") as writer:
-            df_final.to_excel(writer, index=False, sheet_name="Integrales_RMN")
-            writer.save()
-
-        st.download_button(
-            label="📥 Descargar integrales en Excel",
-            data=excel_buffer.getvalue(),
-            file_name="integrales_rmn1h.xlsx",
-            mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-        )
-
-
 
 
 

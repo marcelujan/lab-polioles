d7ab741692f87a11b582ec08d6f140331a21dff5
marcelujan <marcelo.lujan@mi.unc.edu.ar>
2025-05-20 13:42:49 -0300

    Actualizaci├│n autom├ítica desde script

--- tabs_tab6_rmn.py
+++ tabs_tab6_rmn.py
@@ -362,13 +362,14 @@ def render_tab6(db, cargar_muestras, guardar_muestra, mostrar_sector_flotante):
                     if etiqueta and not np.isnan(delta):
                         ax.axvline(x=delta, color="black", linestyle="dashed", linewidth=1)
                         ax.text(
-                            delta,
-                            ax.get_ylim()[1]*0.95,
+                            delta + 0.03,                     # un poco a la derecha de la línea
+                            ax.get_ylim()[1] * 0.95,          # parte superior
                             etiqueta,
-                            rotation=90,
-                            va="top", ha="center",
-                            fontsize=7, color="gray"
+                            rotation=90,                       # vertical
+                            va="top", ha="left",
+                            fontsize=5, color="black"
                         )
+
                 except:
                     continue
 

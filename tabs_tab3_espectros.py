aefffad711e954f000e3080db8304a7aa2f49680
marcelujan <marcelo.lujan@mi.unc.edu.ar>
2025-07-03 15:13:22 -0300

    Actualizaci├│n autom├ítica desde script

--- tabs_tab3_espectros.py
+++ tabs_tab3_espectros.py
@@ -83,7 +83,7 @@ def render_tab3(db, cargar_muestras, guardar_muestra, mostrar_sector_flotante):
                 T2_proy = np.loadtxt(T2_proy_file)
                 ILT2D = np.loadtxt(ILT2D_file)
                 z = ILT2D.T
-                fecha_espectro = st.date_input("Fecha del espectro", value=date.today())
+                fecha_espectro = st.date_input("Fecha del espectro", value=date.today(), key="fecha_rmn1h_t2")
                 observaciones = st.text_area("Observaciones", key="obs_rmn1h_t2")
 
                 st.markdown("### Vista previa ILT2D")

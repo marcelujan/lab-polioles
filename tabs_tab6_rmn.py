9720fa200d41773ef5db195d938ac9de3099d65c
marcelujan <marcelo.lujan@mi.unc.edu.ar>
2025-07-06 13:27:59 -0300

    Actualizaci├│n autom├ítica desde script

--- tabs_tab6_rmn.py
+++ tabs_tab6_rmn.py
@@ -1852,7 +1852,7 @@ def render_tab6(db, cargar_muestras, guardar_muestra, mostrar_sector_flotante):
         # texto antes
         if tipo == "RMN 1H":
             st.markdown("""
-            <span style="font-size: 0.85rem;">
+            <span style="font-size: 0.85rem; text-align:center; display:block;">
             <b>RMN 1H: gráfico 1D de intensidad vs desplazamiento químico. Señal de cada grupo de protones en la muestra. Primera identificación química.</b>
             </span>
             """, unsafe_allow_html=True)

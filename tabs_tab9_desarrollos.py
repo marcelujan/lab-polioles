b1fe78a49324f8a34c493fcf570b71628d66e183
marcelujan <marcelo.lujan@mi.unc.edu.ar>
2025-07-15 17:35:50 -0300

    Add input field for synthesis time in render_tab9 function to enhance user data entry and improve session state management.

--- tabs_tab9_desarrollos.py
+++ tabs_tab9_desarrollos.py
@@ -77,6 +77,7 @@ def render_tab9(db, cargar_muestras, mostrar_sector_flotante):
 
     # Sección Muestreo
     st.markdown('Muestreo')
+    tiempo_sintesis = st.text_input('Tiempo de síntesis', value=st.session_state.get('tiempo_sintesis', ''), key='tiempo_sintesis', on_change=guardar_en_firestore)
     tiempo_muestreo = st.text_input('Tiempo de muestreo', value=st.session_state.get('tiempo_muestreo', ''), key='tiempo_muestreo', on_change=guardar_en_firestore)
     tratamiento_muestras = st.text_area('Tratamiento de muestras', value=st.session_state.get('tratamiento_muestras', ''), key='tratamiento_muestras', on_change=guardar_en_firestore, height=220)
 

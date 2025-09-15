42e5b326603d3dcd783bbcb6dba6ddd01273273c
marcelujan <marcelo.lujan@mi.unc.edu.ar>
2025-09-15 14:30:42 -0300

    Actualizaci├│n autom├ítica desde script

--- theme_plotly_tesis.py
+++ theme_plotly_tesis.py
@@ -9,32 +9,38 @@ tesis_template = go.layout.Template(
     layout=go.Layout(
         font=dict(family=FONT_FAMILY, color="black", size=16),
         title=dict(font=dict(family=FONT_FAMILY, color="black", size=18)),
-        paper_bgcolor="white", plot_bgcolor="white",
+        paper_bgcolor="white",
+        plot_bgcolor="white",
         colorway=pc.qualitative.Plotly,
         margin=dict(l=60, r=20, t=40, b=50),
-        legend=dict(bgcolor="rgba(0,0,0,0)", font=dict(size=14),
-                    orientation="h", yanchor="bottom", y=-0.22, xanchor="center", x=0.5),
+        legend=dict(
+            bgcolor="rgba(0,0,0,0)", font=dict(size=14),
+            orientation="h", yanchor="bottom", y=-0.22, xanchor="center", x=0.5
+        ),
         xaxis=dict(
             showgrid=True, gridcolor="#e0e0e0", gridwidth=1,
+            minor=dict(showgrid=True, gridcolor="#f2f2f2", gridwidth=0.7),
             zeroline=False, showline=True, linecolor="black", linewidth=1.5,
             ticks="outside", tickcolor="black", ticklen=6, tickwidth=1.5,
             title=dict(font=dict(color="black")),
-            tickfont=dict(color="black"),
-            minor=dict(showgrid=True, gridcolor="#f2f2f2", gridwidth=0.7),
+            tickfont=dict(color="black")
         ),
         yaxis=dict(
             showgrid=True, gridcolor="#e0e0e0", gridwidth=1,
+            minor=dict(showgrid=True, gridcolor="#f2f2f2", gridwidth=0.7),
             zeroline=False, showline=True, linecolor="black", linewidth=1.5,
             ticks="outside", tickcolor="black", ticklen=6, tickwidth=1.5,
             title=dict(font=dict(color="black")),
-            tickfont=dict(color="black"),
-            minor=dict(showgrid=True, gridcolor="#f2f2f2", gridwidth=0.7),
+            tickfont=dict(color="black")
         ),
+        shapedefaults=dict(line=dict(color="black", width=2)),
+        annotationdefaults=dict(font=dict(color="black")),
+    ),
     data={
         "scatter": [go.Scatter(mode="lines", line=dict(width=2), marker=dict(size=6))],
         "bar": [go.Bar(marker=dict(line=dict(color="black", width=1)))],
     }
-))
+)
 
 def setup_plotly_tesis(default=True):
     pio.templates["tesis"] = tesis_template

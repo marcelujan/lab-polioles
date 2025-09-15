3e8a9c69e8d8018af135610e1757f1441c885204
marcelujan <marcelo.lujan@mi.unc.edu.ar>
2025-09-15 14:15:31 -0300

    Actualizaci├│n autom├ítica desde script

--- theme_plotly_tesis.py
+++ theme_plotly_tesis.py
@@ -1,6 +1,7 @@
 # theme_plotly_tesis.py
 import plotly.graph_objs as go
 import plotly.io as pio
+from plotly import colors as pc
 
 FONT_FAMILY = "Times New Roman"  # cámbialo si tu reglamento pide otro
 
@@ -9,7 +10,7 @@ tesis_template = go.layout.Template(
         font=dict(family=FONT_FAMILY, color="black", size=16),
         title=dict(font=dict(family=FONT_FAMILY, color="black", size=18)),
         paper_bgcolor="white", plot_bgcolor="white",
-        colorway=["#111111", "#404040", "#6b6b6b", "#9e9e9e", "#c7c7c7"],
+        colorway=pc.qualitative.Plotly,
         margin=dict(l=60, r=20, t=40, b=50),
         legend=dict(bgcolor="rgba(0,0,0,0)", font=dict(size=14),
                     orientation="h", yanchor="bottom", y=-0.22, xanchor="center", x=0.5),
@@ -18,16 +19,17 @@ tesis_template = go.layout.Template(
             zeroline=False, showline=True, linecolor="black", linewidth=1.5,
             ticks="outside", tickcolor="black", ticklen=6, tickwidth=1.5,
             title=dict(font=dict(color="black")),
-            tickfont=dict(color="black")
+            tickfont=dict(color="black"),
+            minor=dict(showgrid=True, gridcolor="#f2f2f2", gridwidth=0.7)
         ),
         yaxis=dict(
             showgrid=True, gridcolor="#e0e0e0", gridwidth=1,
             zeroline=False, showline=True, linecolor="black", linewidth=1.5,
             ticks="outside", tickcolor="black", ticklen=6, tickwidth=1.5,
             title=dict(font=dict(color="black")),
-            tickfont=dict(color="black")
+            tickfont=dict(color="black"),
+            minor=dict(showgrid=True, gridcolor="#f2f2f2", gridwidth=0.7))
         ),
-    ),
     data={
         "scatter": [go.Scatter(mode="lines", line=dict(width=2), marker=dict(size=6))],
         "bar": [go.Bar(marker=dict(line=dict(color="black", width=1)))],
